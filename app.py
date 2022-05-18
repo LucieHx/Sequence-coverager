55ee8fe64962bbbd21254ee208cfe8140179767e
Lucie Hrda <HrdaL233@gmail.com>
2022-05-18 10:17:11 +0200

    backup

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from termcolor import colored
+#from termcolor import colored
 import re
 
 from contextlib import contextmanager, redirect_stdout
@@ -71,7 +71,7 @@ def app():
         
     strips = list(soup.stripped_strings) # pure fasta file
 
-    st.write("Your FASTA file:\n\n", strips, "\n")
+    st.write("FASTA file:\n\n", strips, "\n")
 
     strips_string = "".join(str(x) for x in strips) # converts from list to string
 
@@ -79,32 +79,85 @@ def app():
                                                  # and removes header
         
     sequence = "".join(str(x) for x in strips_list)
-    st.write("Your sequence:\n\n", sequence)
+    
+    sequence_for_display = ' '.join([sequence[i:i+60] for i in range(0, len(sequence), 60)]) # necessary to wrapp string
+                                                                                             # so white space are added
+    
+    st.write("Sequence:\n\n", sequence_for_display)
     
     empty = []
 
+    # for peptide in peptides_sequnces:
+        # if peptide in sequence:
+            # m = re.search(peptide, sequence)
+            # sequence = sequence[:m.start()] + "\x1b[44;33m" + peptide + "\x1b[m" + sequence[m.end():]
+        # else: empty.append(peptide)
+        
     for peptide in peptides_sequnces:
         if peptide in sequence:
             m = re.search(peptide, sequence)
-            sequence = sequence[:m.start()] + "\x1b[44;33m" + peptide + "\x1b[m" + sequence[m.end():]
+            sequence = sequence[:m.start()] + "**" + peptide + "**" + sequence[m.end():]
         else: empty.append(peptide)
-
-    #st.write(sequence)
+        
+    sequence_for_display_bold = ' '.join([sequence[i:i+60] for i in range(0, len(sequence), 60)]) # necessary to wrapp string
+                                                                                             # so white space are added
+    st.markdown(sequence)   
+    st.write("Nenamapované peptidy before:\n\n", empty)
     
-    with st_capture(output.code):
-        print(sequence)
-        sleep(1)
-    # sleep(1)
-    # print("World")
+    pokus='<span style="color:blue">' + 'blabla' + '</span>'
+    
+    
+    html_string = '<span style="color:blue"> blabla</span>' + '<span style="color:black"> some text</span>' + '<span style="color:blue"> some text</span>' 
+
+    st.markdown(html_string, unsafe_allow_html=True)
+    st.markdown(pokus, unsafe_allow_html=True)
+ 
+    sequence = "".join(str(x) for x in strips_list)
+    original_sequence = sequence
+    empty = []
+    #sequence = '<span style="color:black">' + sequence + '</span>'
+    
+    
+    for peptide in peptides_sequnces: # check if peptide is at original sequence!!!
+                
+        if peptide in (sequence and original_sequence):
+        
+            m = re.search(peptide, sequence)
+            sequence = sequence[:m.start()] + '<span style="color:blue">' + peptide + '</span>' + sequence[m.end():]
+            
+        if peptide in original_sequence not in sequence: 
+            
+            omit = "(<span style'color:blue'>|</span>)*" # after each letter of peptide
+            m = re.search(omit.join(x for x in peptide), sequence)
+            peptide_location = sequence[m.span()[0]:m.span()[1]]
+                       
+            
+            if "<span style'color:blue'>" and "</span>" in sequence:
+                peptide_location = peptide_location.replace("<span style'color:blue'>", "").replace("</span>", "") # remove
+            
+        
+        else: empty.append(peptide)
+ 
+    st.markdown(sequence, unsafe_allow_html=True)   
+    
+    st.write("Nenamapované peptidy after:\n\n", empty)
+ 
+ 
+    #st.write(sequence)
+    # '<p style="color:Blue;">Original image</p>'
+    
+    # original_title = '<p style="font-family:Courier; color:Blue; font-size: 20px;">Original image</p>'
+    # st.markdown(original_title, unsafe_allow_html=True)
+    
     
     #st.markdown(f'<span style="color:blue">some *blue* text</span>)
     #st.write(f'blabla{#1aa3ff}')
     
-    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-    for color in colors:
-        st.write('<span style="color:%s">%s</span>' % (color, "blb"), unsafe_allow_html=True)
-    #color=["blue"]
-    #st.write('<span style="color:%s">%s</span>' % (color, color), unsafe_allow_html=True)
+    # colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+    # for color in colors:
+        # st.write('<span style="color:%s">%s</span>' % (color, "blb"), unsafe_allow_html=True)
+    # #color=["blue"]
+    # st.write('<span style="color:%s">%s</span>' % (color, color), unsafe_allow_html=True)
 
 
     # scatter matrix plat
