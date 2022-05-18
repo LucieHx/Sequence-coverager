86d39018dc63e99e72b36f4ced2533a79fa6e21a
Lucie Hrda <HrdaL233@gmail.com>
2022-05-18 15:37:02 +0200

    updated

--- app.py
+++ app.py
@@ -30,24 +30,80 @@ def app():
     st.title("Sequence coverage")
     
     # vstup 1: výběr datové sady
-    data_file_path = st.file_uploader("Data file")
+    data_file_path = st.file_uploader("Upload peptides.txt: ")
     data = None
+   
+    columns = ['Leading razor protein', 'Sequence'] # columns to be loaded
     
-    columns = ['Leading razor protein', 'Sequence']
-    
+    # if data_file_path.name is not ["peptides.txt"]:
+        # st.warning("This is not peptides.txt!!! Please get me aprropritate file!")
+        # return
+        
     if data_file_path is not None:
+    
+        # check column names:
+        
+        col = pd.read_csv(data_file_path, sep='\t', nrows=0).columns.tolist()
+        st.write(col)
+        
+        
+        if ("Leading razor protein" not in col) or ("Sequence" not in col):
+            st.warning("You loaded a wrong file! File must include **Leading razor protein** and **Sequence** columns!")
+            return
+        else: st.write("File seems to be OK!")
+        
+        
+       
+            
+        # seek back to position 0 after reading 
+        
+        st.write("File name:", data_file_path.name)
+        st.write(columns)
+        
         # read data if user uploads a file
-        data = pd.read_csv(data_file_path, sep='\t', usecols = columns)
-        # seek back to position 0 after reading
+        data = pd.read_csv(data_file_path, sep='\t', usecols = columns)        
+        
+
+            
+        # seek back to position 0 after reading    
         data_file_path.seek(0)
+        
+        
     if data is None:
         st.warning("No data loaded")
-        return
+        #return
     
     # vstup 2: výběr uniprotid
-    uniprot_id = st.text_input("Enter uniprot ID")
+    #uniprot_id = st.text_input("Enter uniprot ID")
     
-    st.write(uniprot_id)
+    #st.write(uniprot_id)
+    
+    #uniprot_id = st.text_input("Enter uniprot ID")
+    try:
+    
+        uniprot_id = st.text_input("Enter uniprot ID")
+    
+        st.write(uniprot_id)
+    
+        # Create url from uniprot id and access sequence
+        url = 'https://www.uniprot.org/uniprot/' + uniprot_id + '.fasta'
+
+        html = urlopen(url).read()
+
+        soup = BeautifulSoup(html)
+
+        strips = []
+
+        for script in soup(["script", "style"]):
+            script.decompose() 
+        
+        strips = list(soup.stripped_strings) # pure fasta file
+    except:
+        st.error("Please enter a valid input")
+        return
+    
+    if uniprot_id is not None:
+        st.write(uniprot_id)
     
     if uniprot_id is None:
         st.warning("No uniprot id loaded")
@@ -56,19 +112,19 @@ def app():
     # Getting sequences for selected uniprot id (protein)
     peptides_sequnces = data[data["Leading razor protein"] == uniprot_id]["Sequence"]
     
-    # Create url from uniprot id and access sequence
-    url = 'https://www.uniprot.org/uniprot/' + uniprot_id + '.fasta'
+    # # Create url from uniprot id and access sequence
+    # url = 'https://www.uniprot.org/uniprot/' + uniprot_id + '.fasta'
 
-    html = urlopen(url).read()
+    # html = urlopen(url).read()
 
-    soup = BeautifulSoup(html)
+    # soup = BeautifulSoup(html)
 
-    strips = []
+    # strips = []
 
-    for script in soup(["script", "style"]):
-        script.decompose() 
+    # for script in soup(["script", "style"]):
+        # script.decompose() 
         
-    strips = list(soup.stripped_strings) # pure fasta file
+    # strips = list(soup.stripped_strings) # pure fasta file
 
     st.write("FASTA file:\n\n", strips, "\n")
 
@@ -135,18 +191,5 @@ def app():
     st.write("Not found peptides:\n\n", empty)
     st.write("Tady by nic byt nemělo!:\n\n", empty_shouldnt_exist)
  
- 
-    
-    # scatter matrix plat
-    #st.write(px.scatter_matrix(data, dimensions=dimensions, color=color, opacity=opacity))
-
-    # výběr sloupce pro zobrazení rozdělení dat
-    #interesting_column = st.selectbox("Interesting column", data.columns)
-    # výběr funkce pro zobrazení rozdělovací funkce
-    #dist_plot = st.selectbox("Plot type", [px.box, px.histogram, px.violin])
-
-    #st.write(dist_plot(data, x=interesting_column, color=color))
-
-
 if __name__ == "__main__":
     app()
